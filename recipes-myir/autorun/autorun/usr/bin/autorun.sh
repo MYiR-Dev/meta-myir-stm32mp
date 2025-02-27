74666dd10bcc031681d78d2955bcdfaaa687de1b
beste <beste.wang@myir.cn>
2025-02-27 13:50:47 +0800

    FIX: Update autorun package description and enhance startup script - Correct metadata in autorun.bb - Add NTP servers to timesyncd config - Configure LVDS touchscreen calibration

--- recipes-myir/autorun/autorun/usr/bin/autorun.sh
+++ recipes-myir/autorun/autorun/usr/bin/autorun.sh
@@ -1,23 +1,58 @@
 #!/bin/sh
 source /etc/profile.d/weston_profile.sh
 source /etc/profile.d/pulse_profile.sh
-if [ -e /usr/share/calibrator.txt ]; then
-    test=$(cat /usr/share/calibrator.txt)
 
-    if [ -z "$test" ]; then
-        rm /usr/share/calibrator.txt
-        echo "Del"
-    else
-        echo "OK"
-    fi
+CONFIG_FILE="/etc/systemd/timesyncd.conf"
+TARGET_LINE="FallbackNTP=ntp.ntsc.ac.cn cn.ntp.org.cn time1.google.com time2.google.com time3.google.com time4.google.com"
+
+# Check if target configuration already exists
+if grep -q "^${TARGET_LINE}" "${CONFIG_FILE}"; then
+    echo "Configuration already exists, no changes needed."
+    exit 0
+fi
+
+# Create backup of original file
+cp "${CONFIG_FILE}" "${CONFIG_FILE}.bak"
+
+# Process commented FallbackNTP line
+sed -i -E \
+    -e '/^#FallbackNTP=/ {
+        s/^#//;
+        s/(=).*/\1ntp.ntsc.ac.cn cn.ntp.org.cn time1.google.com time2.google.com time3.google.com time4.google.com/;
+    }' "${CONFIG_FILE}"
+
+# Verify modification
+if grep -q "^${TARGET_LINE}" "${CONFIG_FILE}"; then
+    echo "Successfully updated existing configuration"
 else
-    echo "None!"
+    # Add new configuration to [Time] section
+    if grep -q '^$$Time$$' "${CONFIG_FILE}"; then
+        sed -i '/^$$Time$$/a '"${TARGET_LINE}" "${CONFIG_FILE}"
+        echo "Added new configuration to [Time] section"
+    else
+        echo "[Time] section not found in config file!"
+        exit 1
+    fi
 fi
-sleep 3
-if [ ! -e "/usr/share/calibrator.txt" ];then
-weston-touch-calibrator -v /sys/devices/platform/soc@0/42080000.rifsc/40140000.i2c/i2c-3/3-0038/input/input1/event1 > /usr/share/calibrator.txt
-calibrationvalue=`cat /usr/share/calibrator.txt | awk -F ':' '{print $2}'`
-echo 'SUBSYSTEM=="input", KERNEL=="event[0-9]*", ENV{ID_INPUT_TOUCHSCREEN}=="1", ENV{LIBINPUT_CALIBRATION_MATRIX}="'$calibrationvalue'"' >> /etc/udev/rules.d/touchscreen.rules
+
+# Restart time synchronization service
+systemctl restart systemd-timesyncd.service
+echo "Time synchronization service restarted"
+
+RULES_FILE="/etc/udev/rules.d/touchscreen.rules"
+RULE_LINE='SUBSYSTEM=="input", KERNEL=="event[0-9]*", ENV{ID_INPUT_TOUCHSCREEN}=="1", ENV{LIBINPUT_CALIBRATION_MATRIX}=" 61.509373 0.480948 0.019359 2.795640 116.671989 -0.044791"'
+
+if [ ! -f "$RULES_FILE" ]; then
+    touch "$RULES_FILE"
 fi
+
+if ! grep -qF -- "$RULE_LINE" "$RULES_FILE"; then
+    echo "$RULE_LINE" >> "$RULES_FILE"
+else
+	echo "OK"
+fi
+
 sync
+udevadm control --reload
+udevadm trigger --action=change --subsystem-match=input
 /usr/sbin/mxapp2 &
